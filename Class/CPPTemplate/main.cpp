cd6a4e73a07f6d3a1803881520de1260adb8a702
Brandon Smith <107975382+BSmith311@users.noreply.github.com>
2022-08-28 11:58:29 -0700

    Add files via upload

---
+++ Class/CPPTemplate/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Brandon Smith
+ * Created on August 23, 2022
+ * Purpose: C++ Template - To be used in all future Assignments
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map inputs to outputs -> The Process
+    
+    //Display Results
+    
+    //Exit stage right
+        return 0;
+}
+
